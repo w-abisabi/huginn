a2da3098ef2e65c4f69ca451f79950a29ecfbb69
Marijn Baar <marijn.baar@gmail.com>
2020-08-18 11:24:13 +0200

    now with saving

--- reducers/reducer.js
+++ reducers/reducer.js
@@ -11,6 +11,30 @@ const initialState = {
   ]
 }
 
+const placeReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_PLACE':
+      return [
+        ...state,
+        {
+          id: action.id, 
+          city: action.city, 
+          description: action.description,
+          title: action.title
+        }
+      ]
+    case 'EDIT_PLACE':
+      return state.map(place => (
+        (place.id === action.id)
+        ? {
+          ...place, 
+
+        }
+        : place
+      ))
+  }
+}
+
 const rootReducer = combineReducers({
   
 });
