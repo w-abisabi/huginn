52eb7ebd1cfb46090b7a943fce935681b6b5ee30
Patryk <p.jachimowski89@gmail.com>
2020-08-19 09:29:44 +0200

    Mongo DB schema Added. Mongo DB Atlas conected successfully

--- api/api.js
+++ api/api.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
+
+app.use(cors());
 
 router.get('/', (req, res) => {
   res.json({
@@ -12,11 +15,15 @@ router.get('/', (req, res) => {
 });
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database succesfully connected, yay!')
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('MongoDB database succesfully connected, yay!');
+});
+
+db.on('error', err => {
+  console.error('connection error, meh!', err)
 })
 
 app.use('/.netlify/functions/api', router);
---
+++ models/place.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  city: { type: String, required: true },
+  title: { type: String, required: true },
+}, {
+  timestamps: true,
+});
+
+const Place = mongoose.model('Place', placeSchema);
+
+module.exports = Place;
