eac061a1e803122261165074e3b8f24aa753a449
Patryk <p.jachimowski89@gmail.com>
2020-08-19 15:34:23 +0200

    Storing fetch data in state

--- src/components/MyPlaces.jsx
+++ src/components/MyPlaces.jsx
@@ -1,40 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 // import { useSelector } from 'react-redux';
-import fetchData from '../actions/fetch'
+// import fetchData from '../actions/fetch'
 
-const MyPlaces = () => {
+class MyPlaces extends Component {
   // const places = useSelector(state => state.places);
-  const [places, setPlaces] = useState([]);
+  state = {
+    id: '',
+    city: '',
+    description: '',
+    title: '',
+    places: [],
+  };
 
-  useEffect(() => {
-    async function wrapperFn() {
-      setPlaces(fetchData('places').then());
-    }
-    wrapperFn();
-  }, []);
+  componentDidMount = () => {
+    this.fetchData();
+  };
+
+  // LAST vesion -----------------
+  // const [places, setPlaces] = useState([]);
+  // useEffect(() => {
+  //   async function wrapperFn() {
+  //     setPlaces(fetchData('places').then());
+  //   }
+  //   wrapperFn();
+  // }, []);
+  // LAST vesion -----------------
 
   // try {
   //   setPlaces(fetchData('places'));
   // } catch (error) {
   //   console.log('There was an error:', error);
   // }
-  
-  return (
-    <div className="header">
-      <h1>MyPlaces </h1>
-      {places.length
-        ? places.map(place => (
-          <div key={place._id}>
-            <p>{place.username}</p>
-            <p>{place.city}</p>
-            <p>{place.description} </p>
-            <p>{place.title} </p>
-          </div>
-        ))
-        : <p>No places added yet</p>}
+ 
+  fetchData = async () => {
+    await fetch('/.netlify/functions/api/places', {
+    headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+    }})
+      .then((res) => {
+        const data = res.data;
+        this.setState({ places: data });
+        console.log('Data is here');
+      })
+      .catch(() => {
+        alert('Error meh meh meh :( ');
+      });
+  };
 
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="header">
+        <h1>MyPlaces </h1>
+        {this.state.places}
+        {/* {places.length ? (
+          places.map((place) => (
+            <div key={place._id}>
+              <p>{place.username}</p>
+              <p>{place.city}</p>
+              <p>{place.description} </p>
+              <p>{place.title} </p>
+            </div>
+          ))
+        ) : (
+          <p>No places added yet</p>
+        )} */}
+      </div>
+    );
+  }
+}
 
 export default MyPlaces;
