59c306b3b6b779b8a5b3be62de8829c4e3721ab3
AuraMendez <auralecomte@gmail.com>
2020-08-23 23:01:48 +0200

    multiple photos are possible

--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Memory(props) {
-  const [memory, setMemory] = useState({});
+  const [memory, setMemory] = useState();
   const id = props.match.params.id;
+  let i = 0;
 
-  
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(`/.netlify/functions/api/memories/${id}`, {
@@ -14,9 +14,9 @@ function Memory(props) {
           Accept: 'application/json',
         },
       });
-  
+
       const data = await response.json();
-      console.log('Data is here', data);
+      // console.log('Data is here', data);
       setMemory(data);
     };
     fetchData();
@@ -25,17 +25,26 @@ function Memory(props) {
   return (
     <div>
       <div className="memory">
-      <p className="date">{memory.date}</p>
-        <h2 className="memory-ttl">{memory.title}</h2>
-        <div>
-        <img
-            className="artist-img-big"
-            src={memory.photos}
-            alt="my memory" />
-        </div>
-        <hr/>
-        <h3>{memory.city}</h3>
-        <p>{memory.description}</p>
+        {memory
+          ? (
+            <div>
+              <p className="date">{memory.date}</p>
+              <h2 className="memory-ttl">{memory.title}</h2>
+              <div>
+                {memory.photos.map(photo => (
+                  <img
+                    className="artist-img-big"
+                    src={photo}
+                    alt="my memory" 
+                    key={`${id}_${i++}`}/>
+                ))}
+              </div>
+              <hr />
+              <h3>{memory.city}</h3>
+              <p>{memory.description}</p>
+            </div>
+          )
+          : null}
         <Link className="back-btn" to={'/'}>
           <i className="fas fa-arrow-left"></i> BACK
         </Link>
