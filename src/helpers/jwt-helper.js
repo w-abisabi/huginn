e3b98a75d4a51fc285610b41e2a1ea743f09ec72
Marijn Baar <marijn.baar@gmail.com>
2020-08-24 11:46:05 +0200

    added the key to Netlify

---
+++ src/helpers/jwt-helper.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+
+function createJwtCookie(userId, email) {
+  const secretKey =
+    "-----BEGIN RSA PRIVATE KEY-----\n" +
+    process.env.JWT_SECRET_KEY +
+    "\n-----END RSA PRIVATE KEY-----";
+
+  const token = jwt.sign({ userId, email }, secretKey, {
+    algorithm: "RS256",
+    expiresIn: "100 days"
+  });
+
+  const jwtCookie = cookie.serialize("jwt", token, {
+    secure: process.env.NETLIFY_DEV !== "true",
+    httpOnly: true,
+    path: "/"
+  });
+
+  return jwtCookie;
+}
+
+function clearCookie() {
+  return "jwt=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+}
+
+export { createJwtCookie, clearCookie };
