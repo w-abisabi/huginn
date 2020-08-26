376a920319b5288e945bf3326a53de4194de2069
AuraMendez <auralecomte@gmail.com>
2020-08-26 09:01:48 +0200

    fixing functionality to MyPlaces

--- src/components/MyPlaces.jsx
+++ src/components/MyPlaces.jsx
@@ -1,40 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import fetchData from '../helpers/fetchData'
 
 const MyPlaces = () => {
-  const [memories, setMemories] = useState([]);
+  const [cities, setCities] = useState([]);
 
-  const fetchData = async () => {
-    const response = await fetch('/.netlify/functions/api/memories', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    });
+  
 
-    const data = await response.json()
-    setMemories(data);
-  };
-
-  useEffect(() => {
-    fetchData();
+  useEffect( async () => {
+    const data = await fetchData('GET', 'memories/cities');
+    const parsedPlaces = await response.json(data)
+    setCities(parsedPlaces);
   }, []);
 
-    return (
-      <div className="my-memories">
-        <h2 className="my-memories">My Memories </h2>
-        <hr/>
-       
-        {memories.length 
-        ? memories.map(memory => (
+  return (
+    <div className="my-places">
+      <h2 className="my-places">Where I've been </h2>
+      <hr />
+
+      {memories.length
+        ? cities.map(city => (
 
 
-            <div className="button_mem" align="center" key={memory._id}>
-              <Link className="memory-link" to={`/memory/${memory._id}`}>{`${memory.city}, ${memory.country}`}</Link>
-            </div>
-          )) : <p>You don't have any memories yet.</p>}
-      </div>
-    );
+          <div className="button_mem" align="center" key={memory._id}>
+            <Link className="memory-link" to={`/memory/${memory._id}`}>{`${memory.city}, ${memory.country}`}</Link>
+          </div>
+        )) : <p>You don't have any memories yet.</p>}
+    </div>
+  );
 }
 
 export default MyPlaces;
