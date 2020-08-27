5c1a284e26ebdba666bfe0a70ac5f5366fd7f6d4
Patryk <p.jachimowski89@gmail.com>
2020-08-27 17:34:39 +0200

    Footer starting

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { useAuth } from '../providers/auth-provider';
+
 
 function Navigation() {
+  const { logout } = useAuth();
   return (
     <Navbar variant="dark" className="nav-general" expand="lg">
       <Navbar.Brand>
@@ -12,16 +15,16 @@ function Navigation() {
           <i className="fas fa-plus"></i>
         </Link>
       </Navbar.Brand>
-      <Link to="/">
+      <Nav.Link href="/">
         <p className="logo"></p>
-      </Link>
+      </Nav.Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">home</Nav.Link>
           <Nav.Link href="/new">add memory</Nav.Link>
           <Nav.Link href="/about">about</Nav.Link>
-          <Nav.Link href="/login">logout</Nav.Link>
+          <Nav.Link href="/login" onClick={logout}> logout <i className="fas fa-sign-out-alt"></i></Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
