ca9ff07699c2db9a510b0c495982ddbbc181bf26
Patryk <p.jachimowski89@gmail.com>
2020-08-27 17:16:19 +0200

    nav bar bootstrap

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 function Navigation() {
-
-
-
   return (
-    <header>
-        <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Link to="/new" className="add-btn">
-        <i className="fas fa-plus"></i>
+    <Navbar variant="dark" className="nav-general" expand="lg">
+      <Navbar.Brand>
+        {' '}
+        <Link to="/new" className="add-btn">
+          <i className="fas fa-plus"></i>
+        </Link>
+      </Navbar.Brand>
+      <Link to="/">
+        <p className="logo"></p>
       </Link>
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/login">Logout</Nav.Link>
-    </Nav>
-  </Navbar>
-    </header>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">home</Nav.Link>
+          <Nav.Link href="/new">add memory</Nav.Link>
+          <Nav.Link href="/about">about</Nav.Link>
+          <Nav.Link href="/login">logout</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
