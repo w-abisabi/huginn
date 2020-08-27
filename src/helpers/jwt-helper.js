85fae2fcbe71647f47bdfcaec9b652d67083359b
Gabriel Hirlav <gabriel.hirlav@gmail.com>
2020-08-27 15:55:06 +0200

    took out the coverphoto

--- src/helpers/jwt-helper.js
+++ src/helpers/jwt-helper.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const cookie = require ("cookie");
 
-function createJwtCookie(userId, email, coverphoto) {
+function createJwtCookie(userId, email) {
   const secretKey =
     "-----BEGIN RSA PRIVATE KEY-----\n" +
     process.env.JWT_SECRET_KEY +
     "\n-----END RSA PRIVATE KEY-----";
 
-  const token = jwt.sign({ userId, email, coverphoto }, secretKey, {
+  const token = jwt.sign({ userId, email }, secretKey, {
     algorithm: "RS256",
     expiresIn: "100 days"
   });
