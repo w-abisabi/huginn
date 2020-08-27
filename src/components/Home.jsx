224bcd01d28a5a3253edbefafcf7b3c13c893c94
Patryk <p.jachimowski89@gmail.com>
2020-08-27 09:58:32 +0200

    cover photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
   };
 
   const coverphoto =
-    'https://i.ibb.co/G3VyVLH/41323119-10205298342934849-1734228371589562368-n.jpg'; // change to user.coverphoto after fixing mongo
+    'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80'; // change to user.coverphoto after fixing mongo
 
   useEffect(() => {
     const getNCountries = async () => {
